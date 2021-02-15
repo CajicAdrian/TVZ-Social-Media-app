6303505174ad486d844d4f6d5dcd0ae4eeed0ceb
Chaya <dajjbt3@gmail.com>
2021-02-15 22:16:41 +0100

    Home, Register, Login, Navbar

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,10 +1,53 @@
 import React from 'react';
-import { Text } from '@chakra-ui/react';
+import { Box, Heading, Container, Text, Button, Stack } from '@chakra-ui/react';
 
 export const Home = (): JSX.Element => {
   return (
     <>
-      <Text as="h1">Start here</Text>
+      <Container gridColumn="12 span" minW={'50vw'} maxW={'3:1'}>
+        <Stack
+          as={Box}
+          textAlign={'center'}
+          spacing={{ base: 8, md: 14 }}
+          py={{ base: 20, md: 36 }}
+        >
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
+            lineHeight={'110%'}
+          >
+            Share content with <br />
+            <Text as={'span'} color={'green.400'}>
+              your friends
+            </Text>
+          </Heading>
+          <Text color={'gray.500'}>
+            Share your posts and pictures with others.
+          </Text>
+          <Stack
+            direction={'column'}
+            spacing={3}
+            align={'center'}
+            alignSelf={'center'}
+            position={'relative'}
+          >
+            <Button
+              colorScheme={'green'}
+              bg={'green.400'}
+              rounded={'full'}
+              px={6}
+              _hover={{
+                bg: 'green.500',
+              }}
+            >
+              Register
+            </Button>
+            <Button variant={'link'} colorScheme={'blue'} size={'sm'}>
+              Learn more
+            </Button>
+          </Stack>
+        </Stack>
+      </Container>
     </>
   );
 };
--- src/pages/Register.page.tsx
+++ src/pages/Register.page.tsx
@@ -1,10 +1,67 @@
 import React from 'react';
-import { Text } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Link,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
 
 export const Register = (): JSX.Element => {
   return (
-    <>
-      <Text as="h1">Register</Text>
-    </>
+    <Flex
+      minH={'100vh'}
+      minW={'100vw'}
+      align={'center'}
+      justify={'center'}
+      bg={'gray.50'}
+    >
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Stack align={'center'}>
+          <Heading fontSize={'4xl'}>Register a new account</Heading>
+          <Text fontSize={'lg'} color={'gray.600'}>
+            to enjoy all of our cool <Link color={'blue.400'}>features</Link>
+          </Text>
+        </Stack>
+        <Box rounded={'lg'} bg={'white'} boxShadow={'lg'} p={8}>
+          <Stack spacing={4}>
+            <FormControl id="username">
+              <FormLabel>Username</FormLabel>
+              <Input type="username" />
+            </FormControl>
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Password</FormLabel>
+              <Input type="password" />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Repeat Password</FormLabel>
+              <Input type="password" />
+            </FormControl>
+
+            <Stack spacing={10}>
+              <Button
+                bg={'blue.400'}
+                color={'white'}
+                _hover={{
+                  bg: 'blue.500',
+                }}
+              >
+                Sign up
+              </Button>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
   );
 };
