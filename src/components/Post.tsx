cb77b1c07de9283f19eaf595a7ad2504c5ff9756
Chaya <dajjbt3@gmail.com>
2021-02-18 00:31:50 +0100

    posts showing after signup

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -2,21 +2,25 @@ import React from 'react';
 import {
   Box,
   Center,
-  Heading,
   Text,
   Stack,
-  Avatar,
   useColorModeValue,
   Image,
 } from '@chakra-ui/react';
 
+interface Image {
+  imageId: number;
+  filePath: string;
+  fileName: string;
+}
 interface Props {
-  image: string;
+  image: Image;
   title: string;
   description: string;
 }
 
 export function Post(props: Props): JSX.Element {
+  const { description, image, title } = props;
   return (
     <Center py={6}>
       <Box
@@ -38,9 +42,7 @@ export function Post(props: Props): JSX.Element {
         >
           <Image
             h={'210px'}
-            src={
-              'https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
-            }
+            src={`http://localhost:3000/images/post-images/${image.fileName}`}
             layout={'fill'}
           />
         </Box>
@@ -52,31 +54,9 @@ export function Post(props: Props): JSX.Element {
             fontSize={'sm'}
             letterSpacing={1.1}
           >
-            Blog
+            {title}
           </Text>
-          <Heading
-            color={useColorModeValue('gray.700', 'white')}
-            fontSize={'2xl'}
-            fontFamily={'body'}
-          >
-            Boost your conversion rate
-          </Heading>
-          <Text color={'gray.500'}>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum.
-          </Text>
-        </Stack>
-        <Stack mt={6} direction={'row'} spacing={4} align={'center'}>
-          <Avatar
-            src={'https://avatars0.githubusercontent.com/u/1164541?v=4'}
-            alt={'Author'}
-          />
-          <Stack direction={'column'} spacing={0} fontSize={'sm'}>
-            <Text fontWeight={600}>Achim Rolle</Text>
-            <Text color={'gray.500'}>Feb 08, 2021 · 6min read</Text>
-          </Stack>
+          <Text color={'gray.500'}>{description}</Text>
         </Stack>
       </Box>
     </Center>
