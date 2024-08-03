eec2cac1b42be54a6fd6be7c1d2ca0b58fc100d4
Chaya <dajjbt3@gmail.com>
2024-08-03 19:11:29 +0200

    navbar fix up

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,21 +4,13 @@ import {
   Flex,
   Text,
   Button,
-  Stack,
-  Collapse,
   Icon,
-  Link,
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
   useColorModeValue,
   useBreakpointValue,
-  useDisclosure,
   useColorMode,
   Container,
   HStack,
 } from '@chakra-ui/react';
-import { ChevronDownIcon, ChevronRightIcon } from '@chakra-ui/icons';
 import { MdWbSunny } from 'react-icons/md';
 import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import { AuthContext } from './AuthContext';
@@ -36,13 +28,7 @@ export const LogoutButton = (): ReactElement => {
   }, [setAccessToken, navigate]);
 
   return (
-    <Button
-      onClick={logout}
-      variant="ghost"
-      fontSize={'sm'}
-      fontWeight={600}
-      colorScheme="red"
-    >
+    <Button onClick={logout} fontSize={'sm'} fontWeight={600} colorScheme="red">
       {t('logout')}
     </Button>
   );
@@ -50,7 +36,6 @@ export const LogoutButton = (): ReactElement => {
 
 export const Navbar = (): JSX.Element => {
   const { toggleColorMode, colorMode } = useColorMode();
-  const { isOpen } = useDisclosure();
   const { accessToken } = useContext(AuthContext);
   const { t } = useTranslation();
 
@@ -65,7 +50,9 @@ export const Navbar = (): JSX.Element => {
         px={{ base: 4 }}
         borderBottom={1}
         borderStyle={'solid'}
-        borderColor={useColorModeValue('gray.200', 'gray.900')}
+        borderColor={useColorModeValue('black.200', 'black.900')}
+        borderBottomLeftRadius={'25px'}
+        borderBottomRightRadius={'25px'}
         align={'center'}
       >
         <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
@@ -73,29 +60,20 @@ export const Navbar = (): JSX.Element => {
             textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
             fontFamily={'heading'}
             color={useColorModeValue('gray.800', 'white')}
+            marginLeft={'8.5vw'}
           >
             TBD
           </Text>
         </Flex>
-        <HStack spacing="6">
-          <Box
-            gridColumn="span 12"
-            bg={colorMode === 'dark' ? 'gray.800' : 'white.100'}
-            px="5"
-            py="2"
-            d="flex"
-            alignItems="center"
-          >
-            <Container>
-              <Box>
-                <Icon
-                  as={MdWbSunny}
-                  cursor="pointer"
-                  onClick={toggleColorMode}
-                />
-              </Box>
-            </Container>
-          </Box>
+
+        <Box bg={colorMode === 'dark' ? 'gray.800' : 'white.100'} px="5" py="2">
+          <Container>
+            <Box>
+              <Icon as={MdWbSunny} cursor="pointer" onClick={toggleColorMode} />
+            </Box>
+          </Container>
+        </Box>
+        <HStack spacing="10" marginRight={'8.5vw'}>
           <Box>
             <LanguageMenu />
           </Box>
@@ -112,17 +90,13 @@ export const Navbar = (): JSX.Element => {
               >
                 {t('sign_in')}
               </Button>
+              <Text>/</Text>
               <Button
-                display={{ base: 'none', md: 'inline-flex' }}
                 fontSize={'sm'}
-                fontWeight={600}
-                color={'white'}
-                bg={'pink.400'}
+                fontWeight={400}
                 as={RouterLink}
                 to="/register"
-                _hover={{
-                  bg: 'pink.300',
-                }}
+                variant={'link'}
               >
                 {t('sign_up')}
               </Button>
@@ -130,206 +104,6 @@ export const Navbar = (): JSX.Element => {
           )}
         </HStack>
       </Flex>
-
-      <Collapse in={isOpen} animateOpacity>
-        <MobileNav />
-      </Collapse>
     </Box>
   );
 };
-const DesktopNav = () => {
-  return (
-    <Stack direction={'row'} spacing={4}>
-      {NAV_ITEMS.map((navItem) => (
-        <Box key={navItem.label}>
-          <Popover trigger={'hover'} placement={'bottom-start'}>
-            <PopoverTrigger>
-              <Link
-                p={2}
-                href={navItem.href ?? '#'}
-                fontSize={'sm'}
-                fontWeight={500}
-                color={useColorModeValue('gray.600', 'gray.200')}
-                _hover={{
-                  textDecoration: 'none',
-                  color: useColorModeValue('gray.800', 'white'),
-                }}
-              >
-                {navItem.label}
-              </Link>
-            </PopoverTrigger>
-
-            {navItem.children && (
-              <PopoverContent
-                border={0}
-                boxShadow={'xl'}
-                bg={useColorModeValue('white', 'gray.800')}
-                p={4}
-                rounded={'xl'}
-                minW={'sm'}
-              >
-                <Stack>
-                  {navItem.children.map((child) => (
-                    <DesktopSubNav key={child.label} {...child} />
-                  ))}
-                </Stack>
-              </PopoverContent>
-            )}
-          </Popover>
-        </Box>
-      ))}
-    </Stack>
-  );
-};
-
-const DesktopSubNav = ({ label, href, subLabel }: NavItem) => {
-  return (
-    <Link
-      href={href}
-      role={'group'}
-      display={'block'}
-      p={2}
-      rounded={'md'}
-      _hover={{ bg: useColorModeValue('pink.50', 'gray.900') }}
-    >
-      <Stack direction={'row'} align={'center'}>
-        <Box>
-          <Text
-            transition={'all .3s ease'}
-            _groupHover={{ color: 'pink.400' }}
-            fontWeight={500}
-          >
-            {label}
-          </Text>
-          <Text fontSize={'sm'}>{subLabel}</Text>
-        </Box>
-        <Flex
-          transition={'all .3s ease'}
-          transform={'translateX(-10px)'}
-          opacity={0}
-          _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
-          justify={'flex-end'}
-          align={'center'}
-          flex={1}
-        >
-          <Icon color={'pink.400'} w={5} h={5} as={ChevronRightIcon} />
-        </Flex>
-      </Stack>
-    </Link>
-  );
-};
-
-const MobileNav = () => {
-  return (
-    <Stack
-      bg={useColorModeValue('white', 'gray.800')}
-      p={4}
-      display={{ md: 'none' }}
-    >
-      {NAV_ITEMS.map((navItem) => (
-        <MobileNavItem key={navItem.label} {...navItem} />
-      ))}
-    </Stack>
-  );
-};
-
-const MobileNavItem = ({ label, children, href }: NavItem) => {
-  const { isOpen, onToggle } = useDisclosure();
-
-  return (
-    <Stack spacing={4} onClick={children && onToggle}>
-      <Flex
-        py={2}
-        as={Link}
-        href={href ?? '#'}
-        justify={'space-between'}
-        align={'center'}
-        _hover={{
-          textDecoration: 'none',
-        }}
-      >
-        <Text
-          fontWeight={600}
-          color={useColorModeValue('gray.600', 'gray.200')}
-        >
-          {label}
-        </Text>
-        {children && (
-          <Icon
-            as={ChevronDownIcon}
-            transition={'all .25s ease-in-out'}
-            transform={isOpen ? 'rotate(180deg)' : ''}
-            w={6}
-            h={6}
-          />
-        )}
-      </Flex>
-
-      <Collapse in={isOpen} animateOpacity style={{ marginTop: '0!important' }}>
-        <Stack
-          mt={2}
-          pl={4}
-          borderLeft={1}
-          borderStyle={'solid'}
-          borderColor={useColorModeValue('gray.200', 'gray.700')}
-          align={'start'}
-        >
-          {children &&
-            children.map((child) => (
-              <Link key={child.label} py={2} href={child.href}>
-                {child.label}
-              </Link>
-            ))}
-        </Stack>
-      </Collapse>
-    </Stack>
-  );
-};
-
-interface NavItem {
-  label: string;
-  subLabel?: string;
-  children?: Array<NavItem>;
-  href?: string;
-}
-
-const NAV_ITEMS: Array<NavItem> = [
-  {
-    label: 'Inspiration',
-    children: [
-      {
-        label: 'Explore Design Work',
-        subLabel: 'Trending Design to inspire you',
-        href: '#',
-      },
-      {
-        label: 'New & Noteworthy',
-        subLabel: 'Up-and-coming Designers',
-        href: '#',
-      },
-    ],
-  },
-  {
-    label: 'Find Work',
-    children: [
-      {
-        label: 'Job Board',
-        subLabel: 'Find your dream design job',
-        href: '#',
-      },
-      {
-        label: 'Freelance Projects',
-        subLabel: 'An exclusive list for contract work',
-        href: '#',
-      },
-    ],
-  },
-  {
-    label: 'Learn Design',
-    href: '#',
-  },
-  {
-    label: 'Hire Designers',
-    href: '#',
-  },
-];
